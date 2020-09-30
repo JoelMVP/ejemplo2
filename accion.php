71c83459c5a06c7ca10b08e26ea648c25a5410ed
JoelMendozaP <joelmendozapaucara@gmail.com>
2020-09-30 17:17:06 -0400

    Tarea 2 completada

--- accion.php
+++ accion.php
@@ -1,40 +1,23 @@
 <?php
-// $Nombre=$_GET["Nombre"];
-// $Apellido=$_GET["Apellido"];
-
-// echo "Hola: ".$Nombre." ".$Apellido;
-// echo "<br />";
-// if(isset($_GET["Aceptar"]))
-//     echo "Presiono Apectar";
-// if(isset($_GET["Cancelar"]))
-//     echo "Presiono Cancelar";
-?>
-<?php
 $Nombre = $_POST["Nombre"];
 $Apellido = $_POST["Apellido"];
 
 echo "Hola: " . $Nombre . " " . $Apellido;
 echo "<br />";
-if (isset($_POST["Aceptar"]))
-    echo "Presiono Apectar";
-if (isset($_POST["Cancelar"]))
-    echo "Presiono Cancelar";
+
+
+$option = $_POST['taskOption'];
+echo "Ubicacion: " . $option;
 
 echo "<br />";
 
 if (isset($_POST['check1'])) {
-    echo "Seleccionada";
-    // Checkbox is selected
+    echo "Checkbox Seleccionada";
+    echo "Valor: Si";
 } else {
-    echo "No Seleccionada";
-    // Alternate code
-}
-
-$option = isset($_POST['taskOption']) ? $_POST['taskOption'] : false;
-if ($option) {
-    echo htmlentities($_POST['taskOption'], ENT_QUOTES, "UTF-8");
-} else {
-    echo "task option is required";
-    exit;
+    echo "Checkbox No Seleccionada ";
+    echo "Valor: No";
 }
 ?>
+<br>
+<a href="form.php">Volver al formulario</a>
